8f36ffb969b9e7c9b53989ad3a8f0b5989d14d09
hengyunabc <hengyunabc@gmail.com>
2021-11-19 14:30:57 +0800

    init

---
+++ src/main/java/com/example/exposeprometheusdemo/ExposePrometheusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.exposeprometheusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposePrometheusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposePrometheusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exposeprometheusdemo/ExposePrometheusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exposeprometheusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExposePrometheusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
